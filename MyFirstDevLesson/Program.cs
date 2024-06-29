560a0f39b1fb8d86555feca561d1091d82755ddb
QuangIT1995 <xquang1995@gmail.com>
2024-06-29 19:40:19 +0700

    first program

---
+++ MyFirstDevLesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstDevLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1 Print a string to the console
+            //hint: Console.WriteLine("Hello World!");
+
+            //Part 2 Get user input and print it to the console
+            //hint: Console.ReadLine();
+
+            //Part 3 Get two inputs from the user and print them on separate lines
+            //hint: use \n to create a new line
+
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Please write your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your age and country: ");
+            int age = int.Parse(Console.ReadLine());
+            string country = Console.ReadLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Hello, my name is " +  name);
+            Console.WriteLine("I'm " + age +"\nI'm from " + country);
+            Console.ReadKey();
+        }
+    }
+}
